f882c06c234680d6f3d22c33fcae54e9be63c502
Bogdan405 <Bogdan405@outlook.com>
2020-12-10 23:48:31 +0200

    Animator Script

---
+++ PolyWar/Assets/Scripts/SentryAnimController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SentryAnimController : MonoBehaviour
+{
+    public Animator anim;
+
+
+    public void AttackAnimation()
+    {
+        anim.Play("Attack");
+    }
+
+    public void DefendAnimation()
+    {
+        anim.Play("Defend");
+    }
+
+    public void DeathAnimation()
+    {
+        anim.Play("Death");
+    }
+
+    public void IdleAnimation()
+    {
+        anim.Play("Idle");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            AttackAnimation();
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            DefendAnimation();
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            DeathAnimation();
+        }
+
+    }
+}
