d6e9dd94f9671bf1a4fa00d8dae5a353904b6778
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2020-12-11 15:14:05 +0200

    Rearanged Photon Env

---
+++ PolyWar/Assets/Scripts/Network/ConnectToMaster.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Realtime;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectToMaster : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected!");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected! Cause:" + cause.ToString());
+    }
+}
---
+++ PolyWar/Assets/Scripts/Network/ConnectToRoom.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+
+
+public class ConnectToRoom : MonoBehaviourPunCallbacks
+{
+    private Text console_text;
+    private Text playerNameText;
+    public GameObject outputText;
+    public GameObject playerName;
+    public GameObject player1Name;
+    public GameObject player2Name;
+
+    public  void Start()
+    {
+        console_text = outputText.GetComponent<Text>();
+        playerNameText = playerName.GetComponent<Text>();
+    }
+    public void OnClickJoinRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+        string nickname = playerNameText.text;
+        if (nickname == null)
+        {
+            nickname = "NoName";
+        }
+        PhotonNetwork.LocalPlayer.NickName = nickname;
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(null, options);
+        console_text.text = "Waiting for opponent...";
+    }
+    public override void OnJoinedRoom()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+            console_text.text = "Joined Room";
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName);
+    }
+}
