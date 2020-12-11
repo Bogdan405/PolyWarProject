b75b3c73bbde5742307ea87a1d61b26847338981
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2020-12-11 16:08:11 +0200

    Finished Photon Overview

---
+++ PolyWar/Assets/Scripts/Network/MessageConsoleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class MessageConsoleScript : MonoBehaviourPunCallbacks
+{
+
+    public GameObject console_message;
+
+    [PunRPC]
+    public void update_console(string msg)
+    {
+        Text console_msg = console_message.GetComponent<Text>();
+        console_msg.text = msg;
+    }
+}
