35a84a14e4cb2d4db1ced0664dbb9329a2c330c6
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2020-12-14 19:58:14 +0200

    Added Main MenuUI and all the Scenes

---
+++ PolyWar/Assets/Scripts/Network/ConnectToMasterPun.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Realtime;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ConnectToMasterPun : MonoBehaviourPunCallbacks
+{
+    public
+    void Start()
+    {
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "eu";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        SSTools.ShowMessage("Connected to Server!", SSTools.Position.bottom,SSTools.Time.oneSecond);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        SSTools.ShowMessage("Cannot connect to Server!", SSTools.Position.bottom, SSTools.Time.twoSecond);
+        Application.Quit();
+    }
+}
--- PolyWar/Assets/Scripts/Network/Test/ConnectToMaster.cs
+++ PolyWar/Assets/Scripts/Network/Test/ConnectToMaster.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 
 public class ConnectToMaster : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.GameVersion = "0.0.1";
