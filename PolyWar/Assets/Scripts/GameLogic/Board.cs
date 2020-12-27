f90463a5fefd20883ec7a0de85ce9472d6a0da8d
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2020-12-27 23:39:25 +0200

    asd

--- PolyWar/Assets/Scripts/GameLogic/Board.cs
+++ PolyWar/Assets/Scripts/GameLogic/Board.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 using Card;
 using Photon.Realtime;
 using Photon.Pun;
+using GameUserInterface;
+using PlayerTurn;
 
 public class Board : MonoBehaviour
 {
     private CardClass[] enemyCards;
     private CardClass[] personalCards;
     private bool playedCardThisTurn;
-    private GameObject gameUI;
-    private GameObject turn;
+    public GameObject UI;
+    private int turnCounter;
 
     void Start()
     {
         enemyCards = new CardClass[3];
         personalCards = new CardClass[3];
         playedCardThisTurn = false;
-        
+        turnCounter = 0;
 
 
         enemyCards[0] = null;
@@ -58,28 +60,56 @@ public class Board : MonoBehaviour
 
     public void PlayCard()
     {
-
+        //SetPersonalCard(2,Factory(Model.Sentry, Element.Chemical))
+        personalCards[0].Factory(Model.Sentry, Element.Chemical);
+        playedCardThisTurn = true;
+        UI.GetComponent<GameUI>().SetEndTurnButton(true);
     }
 
     public void Fight()
     {
+        PhotonView boardPV = PhotonView.Get(this);
+        boardPV.RPC("HideUI", RpcTarget.All);
+        System.Threading.Thread.Sleep(2000);
 
+        boardPV.RPC("ShowUI", RpcTarget.All);
     }
 
     public void OnClickEndTurn()
     {
-
+        PhotonView boardPV = PhotonView.Get(this);
+        boardPV.RPC("ResetPlayerTurn", RpcTarget.All);
     }
 
     [PunRPC]
     public void HideUI()
     {
+        UI.GetComponent<GameUI>().OnClickHideUI();
+        UI.GetComponent<GameUI>().setShowButton(false);
+    }
 
+    [PunRPC]
+    public void ShowUI()
+    {
+        UI.GetComponent<GameUI>().OnClickShowUI();
+        UI.GetComponent<GameUI>().setShowButton(true);
     }
 
     [PunRPC]
     public void ResetPlayerTurn()
     {
-
+        turnCounter++;
+        if(turnCounter % 2 == 0)
+        {
+            Fight();
+        }
+        else
+        {
+            PhotonView boardPV = PhotonView.Get(this);
+            boardPV.RPC("ChangeTurn", RpcTarget.All);
+        }
+        if(this.GetComponent<Turn>().IsMyTurn()){
+            playedCardThisTurn = false;
+        }
     }
 }
