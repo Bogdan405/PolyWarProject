2a4f0d1f362e0095db90d88d25e784dd10b31e11
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2020-12-27 21:16:21 +0200

    Improved Battlefield UI

--- PolyWar/Assets/Scripts/UI/BattlefieldUI.cs
+++ PolyWar/Assets/Scripts/UI/BattlefieldUI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Card;
 
 public class BattlefieldUI : MonoBehaviour
 {
@@ -11,4 +12,20 @@ public class BattlefieldUI : MonoBehaviour
     public GameObject enemyCard2;
     public GameObject enemyCard3;
 
+    public void UpdateBattleFieldUI(CardClass[] myCards, CardClass[] enemyCards)
+    {
+        UpdateBattlefieldCard(myCards[0], myCard1);
+        UpdateBattlefieldCard(myCards[1], myCard2);
+        UpdateBattlefieldCard(myCards[2], myCard3);
+        UpdateBattlefieldCard(enemyCards[0], enemyCard1);
+        UpdateBattlefieldCard(enemyCards[1], enemyCard2);
+        UpdateBattlefieldCard(enemyCards[2], enemyCard3);
+
+
+    }
+
+    private void UpdateBattlefieldCard(CardClass card,GameObject uicard)
+    {
+        uicard.GetComponent<BattlefieldCardUI>().initCard(card);
+    }
 }
