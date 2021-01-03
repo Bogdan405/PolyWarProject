7bebc0fc4eb03592e0827b73a41330cbc58db8b3
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-03 18:59:18 +0200

    Fixed Damage Values

--- PolyWar/Assets/Scripts/GameLogic/Battle.cs
+++ PolyWar/Assets/Scripts/GameLogic/Battle.cs
@@ -20,45 +20,45 @@ namespace Card {
                 return damage;
 
             if (elementDealing == Element.Elemental && elementReceiving == Element.Undead)
-                return (int) 1.5 * damage;
+                return (int) (1.5 * damage);
 
             if (elementDealing == Element.Elemental && elementReceiving == Element.Automaton)
-                return (int) 1.0 * damage;
+                return (int) (1.0 * damage);
 
             if (elementDealing == Element.Elemental && elementReceiving == Element.Chemical)
-                return (int) 0.5 * damage;
+                return (int) (0.5 * damage);
 
 
             if (elementDealing == Element.Undead && elementReceiving == Element.Automaton)
-                return (int) 1.5 * damage;
+                return (int) (1.5 * damage);
 
             if (elementDealing == Element.Undead && elementReceiving == Element.Chemical)
-                return (int) 1.0 * damage;
+                return (int) (1.0 * damage);
 
             if (elementDealing == Element.Undead && elementReceiving == Element.Elemental)
-                return (int) 0.5 * damage;
+                return (int) (0.5 * damage);
 
 
 
             if (elementDealing == Element.Automaton && elementReceiving == Element.Chemical)
-                return (int) 1.5 * damage;
+                return (int) (1.5 * damage);
 
             if (elementDealing == Element.Automaton && elementReceiving == Element.Elemental)
-                return (int) 1.0 * damage;
+                return (int)(1.0 * damage);
 
             if (elementDealing == Element.Automaton && elementReceiving == Element.Undead)
-                return (int) 0.5 * damage;
+                return (int)(0.5 * damage);
 
         
 
             if (elementDealing == Element.Chemical && elementReceiving == Element.Elemental)
-                return (int) 1.5 * damage;
+                return (int)(1.5 * damage);
 
             if (elementDealing == Element.Chemical && elementReceiving == Element.Undead)
-                return (int) 1.0 * damage;
+                return (int)(1.0 * damage);
 
             if (elementDealing == Element.Chemical && elementReceiving == Element.Automaton)
-                return (int) 0.5 * damage;
+                return (int)(0.5 * damage);
 
             return 0;
         }
