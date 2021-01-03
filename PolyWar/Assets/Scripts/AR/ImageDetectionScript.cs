d7d3cf836c20f613426580acd82a3941b8e24450
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-03 20:00:09 +0200

    Started AR work/image

--- PolyWar/Assets/Scripts/AR/ImageDetectionScript.cs
+++ PolyWar/Assets/Scripts/AR/ImageDetectionScript.cs
@@ -5,14 +5,17 @@ using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
 using Photon.Realtime;
 using Photon.Pun;
-
+using UnityEngine.XR.ARSubsystems;
 public class ImageDetectionScript : MonoBehaviour
 {
     private ARTrackedImageManager _arTrackedImageManager;
     private Dictionary<string, bool> fieldScanned;
     private bool announced_field_scanned = false;
+    public XRReferenceImageLibrary fieldLibrary;
+    public XRReferenceImageLibrary handLibrary;
     public GameObject UI;
     public GameObject connection;
+    public GameObject gameLogic;
 
     private void Awake()
     {
@@ -41,26 +44,27 @@ public class ImageDetectionScript : MonoBehaviour
 
     public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
     {
-        foreach (var trackedImage in args.added)
+        if(announced_field_scanned == false)
         {
-            SSTools.ShowMessage(trackedImage.referenceImage.name + " scanned", SSTools.Position.middle, SSTools.Time.oneSecond);
-            fieldScanned[trackedImage.referenceImage.name] = true;
-        }
-        foreach(string el in fieldScanned.Keys)
-        {
-            if (fieldScanned[el] == false)
+            foreach (var trackedImage in args.added)
             {
-                SSTools.ShowMessage("Please scan "+el, SSTools.Position.bottom, SSTools.Time.oneSecond);
-                return;
+                SSTools.ShowMessage(trackedImage.referenceImage.name + " scanned", SSTools.Position.middle, SSTools.Time.oneSecond);
+                fieldScanned[trackedImage.referenceImage.name] = true;
             }
-        }
-        if (announced_field_scanned == false)
-        {
+            foreach (string el in fieldScanned.Keys)
+            {
+                if (fieldScanned[el] == false)
+                {
+                    SSTools.ShowMessage("Please scan " + el, SSTools.Position.bottom, SSTools.Time.oneSecond);
+                    return;
+                }
+            }
             SSTools.ShowMessage("Field Scanned!", SSTools.Position.middle, SSTools.Time.threeSecond);
             announced_field_scanned = true;
             PhotonView UIPV = PhotonView.Get(connection);
             UIPV.RPC("updateReadyPlayers", RpcTarget.All);
         }
+        
         
     }
 
