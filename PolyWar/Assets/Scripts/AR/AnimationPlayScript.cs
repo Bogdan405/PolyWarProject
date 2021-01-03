a2e512931f0164fcade7242750cd48a21ad7d837
Bogdan405 <Bogdan405@outlook.com>
2021-01-03 22:30:12 +0200

    Added new script for animations

---
+++ PolyWar/Assets/Scripts/AR/AnimationPlayScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Animator ThisAnimator;
+    void playAttack()
+    {
+        ThisAnimator.play("Attack");
+    }
+    void playDefense()
+    {
+        ThisAnimator.play("Defense");
+    }
+    void playIdle()
+    {
+        ThisAnimator.play("Idle");
+    }
+    void playDeath()
+    {
+        ThisAnimator.play("Death");
+    }
+}
+*/
